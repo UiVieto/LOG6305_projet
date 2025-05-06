842fa03d8b38b1a70a80dd09d308b48d995eb940
Huy Viet Nguyen <huy-viet.nguyen@polymtl.ca>
2025-05-06 11:39:24 -0400

    log arrays for logger

--- server/app/utils/logger.ts
+++ server/app/utils/logger.ts
@@ -11,4 +11,25 @@ export class Logger {
         this.fileWriter.write(String(msg));
         this.fileWriter.write('\n');
     }
+
+    logArray(msgArray: any[]) {
+        let messageToWrite = '[\n';
+
+        for (const msg of msgArray) {
+            if (typeof msg === typeof {}) {
+                messageToWrite += '{\n';
+                for (const attribute in msg) {
+                    messageToWrite += `${attribute}: ${String(msg[attribute])}\n`;
+                }
+                messageToWrite += '}\n';
+            } else {
+                messageToWrite += `${String(msg)}\n`;
+            }
+        }
+
+        messageToWrite += ']';
+
+        this.fileWriter.write(String(messageToWrite));
+        this.fileWriter.write('\n');
+    }
 }
